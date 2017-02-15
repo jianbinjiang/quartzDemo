3718c9c84475bc177e6ae87b95f9377f4cf3804a
SNAKE\s-na <593651731@qq.com>
2017-02-15 17:58:55 +0800

    修改

--- src/main/java/quartzDemo/HelloWorld.java
+++ src/main/java/quartzDemo/HelloWorld.java
@@ -7,13 +7,14 @@ import org.apache.log4j.Logger;
  * 
  * @author jiangjianbin
  * @date 创建时间: 2016年2月15日 
- *
+ *   
  */
 public class HelloWorld {
 
 	private static Logger logger = Logger.getLogger(HelloWorld.class);
 
 	public HelloWorld() {
+		System.out.println("哈哈哈");
 	}
 
 	/**
